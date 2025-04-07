1ba128a1a688bcfad798e46c91e35e3e9b9ba87a
AHMEDSAAD01 <102599154+AHMEDSAAD01@users.noreply.github.com>
2025-04-07 14:33:56 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ import datetime
 import os
 
 app = Flask(__name__)
-@app.route('/')
-def home():
-    return "Hello, world!"
 if __name__ == "__main__":
     app.run(debug=True)
 # بيانات المستخدم للتجربة
